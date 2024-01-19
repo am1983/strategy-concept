98cb4839bf917949b30515b39cfe9c0b17bbf3cf
Aaron Marks <me@aaronmarks.dev>
2024-01-18 19:08:54 -0500

    Leaving a note re: return values

--- Library/RequestObjects/BalanceInquiryRequest.cs
+++ Library/RequestObjects/BalanceInquiryRequest.cs
@@ -1,4 +1,9 @@
 public class BalanceInquiryRequest
 {
     // Whatever data is required to get a balance inquiry processed.
+
+    // How do we make this shareable when the data requirements are different
+    // between strategies?
+
+    // Visitor strategy?
 }
